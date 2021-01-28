632c9ff45e82dc7aa7c841ff4515d97f9fc17038
NitrOP7674 <78051141+NitrOP7674@users.noreply.github.com>
2021-01-28 11:59:03 -0800

    version: use minor version instead of patch

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #pragma once
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_PATCH 1
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
 #define VERSION_BUILD 39
 
 #define stringify(a) stringify_(a)
