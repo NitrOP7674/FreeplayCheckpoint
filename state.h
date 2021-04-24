d927d518f1c55ec31a9a3708bb2e7cdde58ec8a3
NitrOP7674 <78051141+NitrOP7674@users.noreply.github.com>
2021-04-24 12:02:41 -0700

    add copyright

--- state.h
+++ state.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #pragma once
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
