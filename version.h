2505df5b6f79e16c4ecada7dc9bc5501b5e5bbe1
NitrOP7674 <78051141+NitrOP7674@users.noreply.github.com>
2022-03-20 12:04:57 -0700

    update minor version

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 2
+#define VERSION_MINOR 5
 #define VERSION_PATCH 0
-#define VERSION_BUILD 529
+#define VERSION_BUILD 530
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
