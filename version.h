5f529a54bfe84f4efeb5624abfba86f035bd410a
NitrOP7674 <78051141+NitrOP7674@users.noreply.github.com>
2024-02-10 10:28:51 -0800

    add missing files, bump version

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 
 #pragma once
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 7
-#define VERSION_PATCH 0
+#define VERSION_MINOR 8
+#define VERSION_PATCH 1
 #define VERSION_BUILD 740
 
 #define stringify(a) stringify_(a)
